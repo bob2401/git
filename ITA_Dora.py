0c06ba86af219f3df835b3b4bfa6c35ea3146b75
Dora <153235668+bob2401@users.noreply.github.com>
2024-01-05 13:16:29 +0100

    Add files via upload
    ITA assignment3 korištenje GitHuba

---
+++ ITA_Dora.py
@@ -0,0 +1,12 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+someNumber=10
+if(someNumber > 0):
+    print("over zero")
+elif(someNumber < 0):
+    print("below zero")
+else:
+    print("zero")
+
+    
+over zero
